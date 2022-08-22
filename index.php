3c99aa4ef26b9ca24fb4144960b40c7340a89c3c
JuhoKangas <jkangas@student.hive.fi>
2022-08-22 14:52:09 +0300

    start of the login authorization

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php
   session_start();
 
-  $_SESSION['logged_in_user'] = "";
+
+
+  if (isset($_SESSION['logged_in_user'])) {
+    echo '<h1>Here we are</h1>';
+  } else {
+    header('Location: /src/login.html');
+  }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Camagru</title>
-    <link rel="stylesheet" href="./styles/style.css" />
-  </head>
-  <body>
-    <?php if ($_SESSION['logged_in_user'] != ""): ?>
-      <h1>Hey what's up <?php echo $_SESSION['logged_in_user'] ?></h1>
-    <?php else: ?>
-      <h1>Hello Stranger</h1>
-    <?php endif; ?>
-  </body>
-</html>
