5170fd8e817ee59617418c0a133ab5f62ee4ec8b
JuhoKangas <jkangas@student.hive.fi>
2022-09-06 15:39:47 +0300

    Add check_email.php

---
+++ src/check_email.php
@@ -0,0 +1,5 @@
+<?php require_once("includes/header.php") ?>
+  <div class="content d-flex align-center">
+    <h1 class="title">Please check your email</h1>
+  </div>
+<?php require_once("includes/footer.php") ?>
