0b817943506566af3a4db37c8bbd28188cdd64af
JuhoKangas <jkangas@student.hive.fi>
2022-09-07 18:09:09 +0300

    add mailing functions and finding user functions

--- src/functions.php
+++ src/functions.php
@@ -14,24 +14,16 @@ function connect(){
   return $conn;
 }
 
-function auth($username, $password) {
-  $res = null;
-  try {
-    $conn = connect();
-    $stmt = $conn->prepare("SELECT * FROM userinfo WHERE username = ?");
-    $stmt->execute([$username]);
-    $user = $stmt->fetch();
+function send_activation_email (string $email, string $unique_token) {
+  $activation_link = "http://localhost:8080/src/auth.php?email=$email&unique_token=$unique_token";
 
-    if ($user && password_verify($password, $user['password'])) {
-      $res = $user;
-    }
-  } catch (PDOException $e) {
-    echo "Verify Error: " . $e->getMessage();
-  }
-  return $res;
+  $subject = "Please activate your account";
+  $body = "Hey! Please follow this link to verify your email! " . PHP_EOL . $activation_link;
+
+  mail($email, $subject, $body);
 }
 
-function createUser($username, $email, $pwd) {
+function createUser(string $username, string $email, string $pwd): void {
   try {
     $conn = connect();
 
@@ -46,13 +38,15 @@ function createUser($username, $email, $pwd) {
 
     $stmt->execute();
 
+    send_activation_email($email, $unique_token);
+
   } catch(PDOException $e){
     echo "Error: " . $e->getMessage();
   }
   $conn = null;
 }
 
-function usernameAvailable($username) {
+function usernameAvailable(string $username): bool {
   try {
     $conn = connect();
     $stmt = $conn->prepare("SELECT username FROM userinfo WHERE username = ?");
@@ -70,7 +64,7 @@ function usernameAvailable($username) {
   $conn = null;
 }
 
-function emailAvailable($email) {
+function emailAvailable(string $email): bool {
   try {
     $conn = connect();
     $stmt = $conn->prepare("SELECT email FROM userinfo WHERE email = ?");
@@ -86,6 +80,59 @@ function emailAvailable($email) {
     echo "emailAvailable Error: " . $e->getMessage();
   }
   $conn = null;
+}
+
+function find_by_username(string $username) {
+  try {
+    $conn = connect();
+    $stmt = $conn->prepare("SELECT username, password, email, activated FROM userinfo WHERE username=:username");
+    $stmt->bindValue(":username", $username);
+    $stmt->execute();
+  
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    echo "find_by_username Error: " . $e->getMessage();
+  }
+  $conn = null;
+}
+
+function user_active(string $username) {
+  $user = find_by_username($username);
+  return (int)$user['activated'] === 1;
+}
+
+function login_user(string $username, string $password): bool {
+  try {
+    $user = find_by_username($username);
+
+    if ($user && password_verify($password, $user['password'])) {
+      // Prevent session fixation attack
+      session_regenerate_id();
+
+      $_SESSION['logged_in_user'] = $user['username'];
+      $_SESSION['user_id'] = $user['id'];
+
+      return true;
+    }
+
+    return false;
+  } catch (PDOException $e) {
+    echo "Login Error: " . $e->getMessage();
+  }
+}
+
+function activate_user($email, $unique_token) {
+  try {
+    $conn = connect();
+    $stmt = $conn->prepare("UPDATE userinfo SET activated=1 WHERE email=:email AND unique_token=:unique_token");
+    $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':unique_token', $unique_token);
+    $stmt->execute();
+
+    return $stmt->rowCount();
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+  }
 }
 
 ?>
