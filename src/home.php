de69d408518853d8caea9a807fe10c135e109c03
Juho Kangas <jkangas@student.hive.fi>
2022-09-12 18:10:57 +0300

    start working on the card, add placeholder image

--- src/home.php
+++ src/home.php
@@ -4,16 +4,25 @@
   //   header("location: login.php");
   // }
 ?>
-  <div class="content px-4">
+  <div class="content px-5">
 
-    <div class="card">
-      <div class="card-top"></div>
+    <div class="card mt-5">
+      <div class="card-header py-2 px-2">
+        <p class="card-username">username</p>
+        <p class="card-time">39m ago</p>
+      </div>
       <div class="card-img"></div>
-      <div class="card-likes"></div>
+        <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+      <div class="card-likes">
+        <i class="fa-regular fa-heart"></i>
+        <i class="fa-solid fa-heart"></i>
+        <i class="fa-regular fa-comment"></i>
+        <i class="fa-solid fa-share-nodes"></i>
+      </div>
       <div class="card-comments-list"></div>
       <div class="card-comment"></div>
     </div>
     
-    <a href="logout.php">logout</a>
+    <!-- <a href="logout.php">logout</a> -->
   </div>
 <?php require_once("includes/footer.php") ?>
--- src/includes/header.php
+++ src/includes/header.php
@@ -15,6 +15,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="../styles/style.css" />
+    <!-- FONT AWESOME -->
+    <script src="https://kit.fontawesome.com/f1f997944d.js" crossorigin="anonymous"></script>
   </head>
   <body>
     <nav>
