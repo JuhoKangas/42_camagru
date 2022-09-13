931c9985ddf6705610682791ddfd3276361513c1
Juho Kangas <jkangas@student.hive.fi>
2022-09-13 16:17:44 +0300

    initial responsive layout for home view

--- src/home.php
+++ src/home.php
@@ -6,36 +6,396 @@
 ?>
   <div class="content">
 
-    <div class="card">
-      <div class="card-header">
-        <p class="card-username">username</p>
-        <p class="card-time">39m ago</p>
-      </div>
-      <div class="card-img"></div>
-        <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
-      <div class="card-likes">
-        <img src="../img/icons/heart_vector.svg" alt="heart">
-        <p>256</p>
-        <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
-        <i class="fa-regular fa-comment"></i>
-        <p>9</p>
-        <i class="fa-solid fa-share-nodes"></i>
-      </div>
-      <div class="card-description">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
-      </div>
-      <div class="card-comments-list">
-        <div class="comment">
-          <p class="card-username">username</p>
-          <p>Lorem ipsum dolor sit amet.</p>
+    <div class="cards-container">
+
+      <div class="card-column">
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
         </div>
-        <div class="comment">
-          <p class="card-username">username</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
         </div>
       </div>
-      <div class="card-comment"></div>
+      
+      <div class="card-column">
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+      </div>
+      
+      <div class="card-column">
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+      </div>
+      
+      <div class="card-column">
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+        <div class="card">
+          <div class="card-header">
+            <p class="card-username">username</p>
+            <p class="card-time">39m ago</p>
+          </div>
+          <div class="card-img"></div>
+            <img src="../img/john-o-nolan-6f_ANCcbj3o-unsplash.jpg" alt="" width="100%">
+          <div class="card-likes">
+            <img src="../img/icons/heart_vector.svg" alt="heart">
+            <p>256</p>
+            <!-- <img src="../img/icons/heart_filled.svg" alt="heart filled"> -->
+            <i class="fa-regular fa-comment"></i>
+            <p>9</p>
+            <i class="fa-solid fa-share-nodes"></i>
+          </div>
+          <div class="card-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum unde molestias et veritatis ut illo, numquam culpa minima aliquam quaerat.</p>
+          </div>
+          <div class="card-comments-list">
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="comment">
+              <p class="card-username">username</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, totam.</p>
+            </div>
+          </div>
+          <div class="card-comment"></div>
+        </div>
+      </div>
+
+      
+      
+    
+      
+      
+    
     </div>
+
     
     <!-- <a href="logout.php">logout</a> -->
   </div>
