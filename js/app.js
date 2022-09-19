ffa31c5b0da977f4c5c20118b5c3fb317d52918d
JuhoKangas <jkangas@student.hive.fi>
2022-09-19 16:46:24 +0300

    write js for uploading a webcam pic

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const video = document.querySelector("#video");
 const click_button = document.querySelector("#click-photo");
 const canvas = document.querySelector("#canvas");
 const stop_button = document.querySelector("#stop-webcam");
+const webcam_picture = document.querySelector('#webcam-picture')
 
 camera_button.addEventListener('click', async () => {
 	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -10,11 +11,13 @@ camera_button.addEventListener('click', async () => {
 });
 
 click_button.addEventListener('click', () => {
-   	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-   	let image_data_url = canvas.toDataURL();
+	if (video.srcObject) {
+		canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+		let image_data_url = canvas.toDataURL();
+	
+		webcam_picture.value = image_data_url;
+	}
 
-   	// data url of the image
-   	console.log(image_data_url);
 });
 
 stop_button.addEventListener('click', () => {
@@ -26,6 +29,6 @@ stop_button.addEventListener('click', () => {
 			track.stop();
 	}
 	video.srcObject = null;
-	
+
 	canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 })
