0b047f4f4fb67b34f88dba366b6129a4d9abda35
JuhoKangas <jkangas@student.hive.fi>
2022-09-20 12:55:41 +0300

    finish uploading from webcam

--- src/upload_webcam.php
+++ src/upload_webcam.php
@@ -3,13 +3,19 @@
 	require_once("functions.php");
 
 	$data_url = $_POST['webcam_picture'];
+	$target_dir = "../img/uploads/";
+	$img_id = bin2hex(random_bytes(5));
+	$filename = $img_id . '.png';
+	$target_file = $target_dir . $filename;
 
 	if ($data_url) {
 		list($type, $data) = explode(';', $data_url);
 		list(, $data) = explode(',', $data);
 	
 		$data = base64_decode($data);
-		file_put_contents('../img/uploads/test.png', $data);
+		file_put_contents($target_file, $data);
+		uploadPicture($_SESSION['user_id'], $filename);
+		header("location: upload.php");
 	} else {
 		echo "empty pic";
 	}
