326af37fae57fbd58744ea98156aee900b3e07ea
JuhoKangas <jkangas@student.hive.fi>
2022-09-21 17:20:31 +0300

    move access denying to .htaccess

--- src/functions.php
+++ src/functions.php
@@ -1,10 +1,5 @@
 <?php
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
-  header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-  die ("<h2>Access Denied!</h2> This file is protected and not available to public.");
-}
-
 $DB_DSN = 'mysql:host=localhost;dbname=camagru_db';
 $DB_USER = 'root';
 $DB_PASSWORD = '123123';
