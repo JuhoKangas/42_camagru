5119f3cbf03573af922b673939d7e001ee1e65f3
JuhoKangas <jkangas@student.hive.fi>
2022-09-21 15:23:44 +0300

    rework the check_email page

--- src/check_email.php
+++ src/check_email.php
@@ -1,5 +1,6 @@
 <?php require_once("includes/header.php") ?>
-  <div class="content d-flex align-center">
-    <h1 class="title">Please check your email</h1>
+  <div class="content d-flex d-column align-center">
+    <img src="../img/office1.png" width="250" alt="">
+    <h1 class="title pt-2">Please check your email</h1>
   </div>
 <?php require_once("includes/footer.php") ?>
