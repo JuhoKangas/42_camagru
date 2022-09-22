cc6190725272ff287ec1fb90694846d994c60bd2
JuhoKangas <jkangas@student.hive.fi>
2022-09-22 18:45:28 +0300

    write javascript to fill the uploaded image to the canvas

--- js/app.js
+++ js/app.js
@@ -3,7 +3,48 @@ const video = document.querySelector("#video");
 const click_button = document.querySelector("#click-photo");
 const canvas = document.querySelector("#canvas");
 const stop_button = document.querySelector("#stop-webcam");
-const webcam_picture = document.querySelector('#webcam-picture')
+const webcam_picture = document.querySelector('#webcam-picture');
+const file_input = document.querySelector("#fileToUpload");
+const picture = document.querySelector("#picture");
+
+const sticker = document.querySelectorAll(".sticker");
+
+for (let i=0; i<sticker.length; i++) {
+	sticker[i].addEventListener('click', e => {
+		console.log(e.target.id);
+
+	})
+}
+
+file_input.onchange = () => {
+	const [file] = file_input.files;
+	if (file) {
+		picture.src = URL.createObjectURL(file);
+
+		// We have to give it some time to think
+		setTimeout(() => {
+			let maxDim = 800;
+			if (picture.width > maxDim || picture.height > maxDim) {
+					let ratio = picture.width/picture.height;
+					if( ratio > 1) {
+							picture.width = maxDim;
+							picture.height = maxDim/ratio;
+							canvas.height = maxDim/ratio;
+							canvas.width = maxDim;
+						} else {
+							picture.width = maxDim*ratio;
+							picture.height = maxDim;
+							canvas.width = maxDim*ratio;
+							canvas.height = maxDim;
+					}
+				}
+				canvas.getContext('2d').drawImage(picture, 0, 0, picture.width, picture.height);
+				let image_data_url = canvas.toDataURL();
+				webcam_picture.value = image_data_url;
+				picture.remove();
+		}, 50);
+	}
+}
 
 camera_button.addEventListener('click', async () => {
 	let stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
