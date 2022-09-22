1fb754fa4677c9c6f665fb57a06c3678e820440a
JuhoKangas <jkangas@student.hive.fi>
2022-09-22 18:46:06 +0300

    write php to resize the image keeping the ratio

--- src/upload.php
+++ src/upload.php
@@ -29,14 +29,32 @@
     if($uploadOk === 0) {
       echo $errMsg;
     } else {
-      if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $targetFile)) {
-        echo "picture uploaded";
-        uploadPicture($_SESSION['user_id'], $filename);
+      // Resize the image before uploading, Do i need this?
+      $maxDim = 800;
+      $file_name = $_FILES['fileToUpload']['tmp_name'];
+      list($width, $height, $type, $attr) = getimagesize($file_name);
+      if ($width > $maxDim || $height > $maxDim) {
+          $ratio = $width/$height;
+          if( $ratio > 1) {
+              $new_width = $maxDim;
+              $new_height = $maxDim/$ratio;
+          } else {
+              $new_width = $maxDim*$ratio;
+              $new_height = $maxDim;
+          }
+        $src = imagecreatefromstring(file_get_contents($file_name));
+        $dst = imagecreatetruecolor($new_width, $new_height);
+        imagecopyresampled($dst, $src, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+        imagedestroy($src);
+        if ($fileType === 'jpg' || $filetype === 'jpeg') {
+          imagejpeg($dst, $targetFile); // adjust format as needed
+        } else if ($fileType === 'png') {
+          imagepng($dst, $targetFile);
+        }
+        imagedestroy( $dst );
       }
-      else {
-        echo "There was a problem uploading the picture";
+      uploadPicture($_SESSION['user_id'], $filename);
       }
-    }
 
   }
 
@@ -47,22 +65,31 @@
 
   <div class="web-photo">
     <button id="start-camera">Start Camera</button>
-    <video id="video" width="320" height="240" autoplay></video>
+    <video id="video" width="500" height="375" autoplay></video>
     <button id="click-photo">Click Photo</button>
-
+    
     <form action="upload_webcam.php" method="post" enctype="multipart/form-data">
       <input name="upload-button" type="submit" value="Submit">
       <input id="webcam-picture" type="hidden" name="webcam_picture" value="">
     </form>
-
+    
     <button id="stop-webcam">Stop webcam</button>
-    <canvas id="canvas" width="320" height="240"></canvas>
+    <img id="picture">
+    <canvas id="canvas" width="500" height="375"></canvas>
   </div>
   
   <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data" >
-    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input accept="image/*" type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload picture" name="submit">
   </form>
+
+  <div class="stickers">
+    <img class="sticker" src="../img/stickers/bee_sticker.png" alt="" id="sticker1">
+    <img class="sticker" src="../img/stickers/best-mom_sticker.png" alt="" id="sticker2">
+    <img class="sticker" src="../img/stickers/corgi_sticker.png" alt="" id="sticker3">
+    <img class="sticker" src="../img/stickers/dontworry_sticker.png" alt="" id="sticker4">
+    <img class="sticker" src="../img/stickers/yass_sticker.png" alt="" id="sticker5">
+  </div>
 </div>
 
 <?php require_once('includes/footer.php'); ?>
