02e0c77963758ba274c67393e40efeefd29596d3
Juho Kangas <jkangas@student.hive.fi>
2022-10-06 14:58:28 +0300

    make stickers always be inside image

--- src/upload_file.php
+++ src/upload_file.php
@@ -137,19 +137,19 @@ const addSticker = (sticker) => {
 
 	switch (sticker) {
 		case "sticker1":
-			ctx.drawImage(selectedSticker, 80, 40, selectedSticker.width, selectedSticker.height)
+			ctx.drawImage(selectedSticker, canvas.width / 4, canvas.height / 2, selectedSticker.width, selectedSticker.height)
 			break;
 		case "sticker2":
-			ctx.drawImage(selectedSticker, 550, 40, selectedSticker.width, selectedSticker.height)
+			ctx.drawImage(selectedSticker,canvas.width / 2, canvas.height / 8, selectedSticker.width, selectedSticker.height)
 			break;
 		case "sticker3":
-			ctx.drawImage(selectedSticker, 80, 400, selectedSticker.width, selectedSticker.height)
+			ctx.drawImage(selectedSticker, canvas.width / 3, canvas.height / 9, selectedSticker.width, selectedSticker.height)
 			break;
 		case "sticker4":
-			ctx.drawImage(selectedSticker, 300, 250, selectedSticker.width, selectedSticker.height)
+			ctx.drawImage(selectedSticker, canvas.width / 4, canvas.height / 5, selectedSticker.width, selectedSticker.height)
 			break;
 		case "sticker5":
-			ctx.drawImage(selectedSticker, 550, 400, selectedSticker.width, selectedSticker.height)
+			ctx.drawImage(selectedSticker, canvas.width / 5, canvas.height / 4, selectedSticker.width, selectedSticker.height)
 			break;
 	
 		default:
