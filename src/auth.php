78f8360be84b7ff9678aa9a6ec6116c17dd8fdf1
Juho Kangas <jkangas@student.hive.fi>
2022-10-06 11:42:08 +0300

    improve styling

--- src/auth.php
+++ src/auth.php
@@ -1,6 +1,6 @@
 <?php
-  require_once("functions.php");
   require_once("includes/header.php");
+  require_once("functions.php");
   
   $email = $_GET['email'];
   $unique_token = $_GET['unique_token'];
@@ -10,9 +10,9 @@
 <?php if(activate_user($email, $unique_token)): ?>
   <div class="content d-flex d-column align-center">
     <img src="../img/office2.png" width="250" alt="">
-    <h1 class="title pt-2">You have been verified!</h1>
-    <p class="mt-4">Thanks for confirming your account! You can now start posting on Camagru.</p>
-    <a href="home.php"><div class="btn btn-main btn-big mt-5">Start Posting</div></a>
+    <h1 class="title py-2">You have been verified!</h1>
+    <p class="my-4">Thanks for confirming your account! You can now start posting on Camagru.</p>
+    <a href="login.php"><div class="btn btn-main btn-big mt-5">Log in!</div></a>
   </div>
 <?php else: ?>
     <div class="content d-flex align-center">
